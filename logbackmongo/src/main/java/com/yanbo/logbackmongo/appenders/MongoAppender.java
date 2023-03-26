7f52fa3ef82dc7012e04f0700de31190f37a1dbc
btparker-work <40068250+btparker-work@users.noreply.github.com>
2023-03-26 05:45:34 -0500

    cleanup

--- logbackmongo/src/main/java/com/yanbo/logbackmongo/appenders/MongoAppender.java
+++ logbackmongo/src/main/java/com/yanbo/logbackmongo/appenders/MongoAppender.java
@@ -6,16 +6,11 @@ import ch.qos.logback.core.status.ErrorStatus;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
-import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoClientURI;
-import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.springframework.util.StringUtils;
-
 import java.util.Date;
 
 /**
